21eb4c64fef91c73b78df72ab928b9d0d8b6d8bd
Ravi-Rohith <90379626+Ravi-Rohith@users.noreply.github.com>
2023-11-17 21:58:14 +0530

    finished ...nect auth

---
+++ src/app/pages/api/[...nextauth].ts
@@ -0,0 +1,41 @@
+import NextAuth from "next-auth/next";
+import Credentials from "next-auth/providers/credentials"
+import { type } from "os";
+import { compare } from 'bcrypt'
+
+import prismadb from '@lib/prismadb'
+
+export default NextAuth({
+    providers: [
+        Credentials({
+            id: "credentials",
+            name: "credentials",
+            credentials: {
+                email: {
+                    label: 'Email',
+                    type: 'text',
+                },
+                password: {
+                    label: 'Password',
+                    type: 'password',
+                }
+            }
+            async authorize(credentials) {
+                if (!credentials?.email || !credentials?.password) {
+                    throw new Error("Email and Password required");
+                }
+
+                const user = await prismadb.user.findUnique({
+                    where: {
+                        email : credentials.email
+                    }
+                })
+                if (!user || !user.hashedPassword) {
+                    throw new Error("Email does not exist")
+                }
+
+                const isCorrectPassword = await compare
+            }
+        })
+    ]
+})
